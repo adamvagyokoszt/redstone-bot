5119ec04ec70b9cc92bf103f9c017ba3f5993637
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-02-12 18:57:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -595,39 +595,7 @@ if(cmd === `${prefix}kick`){
         }
     }
 
-        if(cmd === `${prefix}mute`){
-        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Nincs jogod ezt a parancsot használni!')
-        const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-        if(!Member) return message.channel.send('Akit megjelölzél nem található!')
-        const role = message.guild.roles.cache.find(role => role.name.toLowerCase() === 'muted')
-        if(!role) {
-            try {
-                message.channel.send('Némított rang nem található! Várj amíg elkészül!')
-
-                let muterole = await message.guild.roles.create({
-                    data : {
-                        name : 'muted',
-                        permissions: []
-                    }
-                });
-                message.guild.channels.cache.filter(c => c.type === 'text').forEach(async (channel, id) => {
-                    await channel.createOverwrite(muterole, {
-                        SEND_MESSAGES: false,
-                        ADD_REACTIONS: false
-                    })
-                });
-                message.channel.send('Némított rang kész!')
-            } catch (error) {
-                console.log(error)
-            }
-        };
-        let role2 = message.guild.roles.cache.find(r => r.name.toLowerCase() === 'muted')
-        if(Member.roles.cache.has(role2.id)) return message.channel.send(`${Member.displayName} már le van némítva!`)
-        await Member.roles.add(role2)
-        message.channel.send(`${Member.displayName} Le lett némítva!`)
-    }
-} 
-
+        
 
 })
 bot.login(process.env.TOKEN)
