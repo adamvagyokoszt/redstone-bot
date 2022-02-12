2ad24fadb4535c25eb4e6d812259787c69978453
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-02-12 11:04:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -85,6 +85,41 @@ bot.on("message", async message => {
         }
     }
 
+    if(cmd === `${prefix}pay`){
+        let pay_money = Math.round(args[0]*100)/100
+        if(isNaN(pay_money)) return message.reply(`A parancs helyes használata: ${prefix}pay <összeg> <@név>`)
+        if(pay_money > selfMoney) return message.reply("az egyenlegednél több pénzt nem adhatsz meg!")
+
+        let pay_user = message.mentions.members.first();
+
+        if(args[1] && pay_user){
+            if(!money[pay_user.id]) {
+                money[pay_user.id] = {
+                    money: 100,
+                    user_id: pay_user.id
+                }
+            }
+
+            money[pay_user.id] = {
+                money: money[pay_user.id].money + pay_money,
+                user_id: pay_user.id
+            }
+
+            money[message.author.id] = {
+                money: selfMoney - pay_money,
+                user_id: message.author.id
+        }
+
+        message.channel.send(`Sikeresen átutaltál <@${pay_user.id}> számlájára ${pay_money}FT-ot!`)
+
+        fs.writeFile("./money.json", JSON.stringify(money), (err) => {
+            if(err) console.log(err);
+        });
+    } else {
+        message.reply(`A parancs helyes használata: ${prefix}pay <összeg> <@név>`)
+    }
+}
+
 if(cmd === `${prefix}work`){
     let cd_role_id = "879320935566565438";
     let cooldown_time = "10";
