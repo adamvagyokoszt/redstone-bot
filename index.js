538667e173c710e5398e84f47944e5bf2321c513
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-02-14 17:04:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -381,13 +381,13 @@ if(cmd === `${prefix}macska`){
         const random = subreddits[Math.floor(Math.random() * subreddits.length)]
 
         const IMG = await randomPuppy(random)
-        const MemeEmbed = new Discord.MessageEmbed()
+        const MemeEmbed = new MessageEmbed()
         .setColor("RANDOM")
         .setImage(IMG)
         .setTitle(`Keresési szöveg: ${random} (KATT IDE!)`)
         .setURL(`https://www.reddit.com/r/${random}`)
 
-        message.channel.send(MemeEmbed)
+        message.channel.send({ embeds: [MemeEmbed] })
     } 
 
 
