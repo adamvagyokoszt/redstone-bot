8b84d958775527cfb44bd1504814e8be62a037e9
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-02-14 13:34:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Client, Intents, MessageEmbed, Permissions } = require("discord.js")
-const allIntents = new Intents(32767);
-const client = new Client({ allIntents });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.DIRECT_MESSAGE_TYPING,Intents.FLAGS.GUILD_MEMBERS,Intents.FLAGS.GUILD_PRESENCES,Intents.FLAGS.GUILD_MESSAGES ] });
 const botconfig = require("./botconfig.json")
 const money = require("./money.json")
 var weather = require('weather-js');
