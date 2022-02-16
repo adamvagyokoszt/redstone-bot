3beacb1092d5001ebfd7e59e1da8496719396be5
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-02-16 19:48:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -464,63 +464,85 @@ message.channel.send({ embeds: [embed] })
     
     
 
- if(cmd === `${prefix}giveaway`){
-            const messageArray = message.content.split(" ");
-            if (!message.member.permissions.has('ADMINISTRATOR')) return message.channel.send(`> __Nincs megfelelő engedélyed a parancs használatához!__`);
-            let tárgy = "";
-            let idő;
-            let winnerCount;
  
-            for (let i = 1; i < args.length; i++){
-                tárgy += (args[i] + " ")
-                console.log(tárgy)
-            }
- 
-            idő = args[0];
- 
-        if(!idő){
-            return message.reply("Kérlek adj meg egy idő intervallumot! pl: 100s, 5h, 2d")
-        }
-        if(!tárgy){
-            return message.reply("Kérlek add meg a nyereményjáték tárgyát!")
-        }
- 
-        var Gembed = new MessageEmbed()
-        .setColor("RED")
-        .setTitle("🎉 Nyereményjáték 🎉")
-        .setDescription(`**${tárgy}**`)
-        .addField("`Időtartam:`", ms(ms(idő), {long: true}), true)
-        .setFooter("A jelentkezéshe reagálj ezzel: 🎉")
-        message.channel.send({ embeds: [Gembed] }).then(async msg => {
-            await msg.react("🎉")
-        })
-        
+I guess this giveaway command works for me - Discord.js V13
+
+don't forget to have npm i ms installed if you haven't already
+const { Client, Intents, CommandInteraction, ReactionUserManager } = require('discord.js'); 
+const INTENTS = new Intents(32767); // 32767 == full intents, calculated from intent calculator 
+
+const client = new Client({
+  intents: INTENTS
+});
 
 
-        setTimeout(async() => {
-            try{
-                const peopleReactedBot =  await Gembed.reactions.cache.get("🎉").users.fetch();
-                var peopleReacted = peopleReactedBot.array().filter(u => u.id !== bot.user.id);
-            }catch(e){
-                return message.channel.send(`Hiba törtét a **${tárgy}** sorsolása során! Hiba: `+"`"+e+"`")
-            }
-        
-            var winner;
- 
-            if(peopleReacted.length <= 0){
-                return message.channel.send("Senki nem jelentkezett a nyereményjátékra! :C")
-            } else {
-                var index = Math.floor(Math.random() * peopleReacted.length);
-                winner = peopleReacted[index]
-            }
- 
-            if(!winner) {
-                message.channel.send("Hiba történt a sorsolás során!")
-            } else {
-                message.channel.send(`🎉🎉🎉🎉 **${winner.toString()}** Gratulálok! Nyereményed: **${tárgy}** 🎉🎉🎉🎉`);
-            }
-        }, ms(idő))
-        }
+
+    if ((message.content.startsWith(`${prefix}giveaway`))) { // this condition can be changed to any command you'd like, e.g. `${prefix}gstart`
+        if (message.member.roles.cache.some(role => (role.name === 'Giveaway') )) { // user must have a role named Giveaway to start giveaway
+        let duration = args[1];
+        let winnerCount = args[2];
+
+        if (!duration) 
+            return message.channel.send('Please provide a duration for the giveaway!\nThe abbreviations for units of time are: `d (days), h (hours), m (minutes), s (seconds)`');
+        if (
+            !args[1].endsWith("d") &&
+            !args[1].endsWith("h") &&
+            !args[1].endsWith("m") &&
+            !args[1].endsWith("s") 
+        )
+            return message.channel.send('Please provide a duration for the giveaway!\nThe abbreviations for units of time are: `d (days), h (hours), m (minutes), s (seconds)`');
+
+        if (!winnerCount) return message.channel.send('Please provide the number of winners for the giveaway! E.g. `1w`')
+
+        if (isNaN(args[2].toString().slice(0, -1)) || !args[2].endsWith("w")) // if args[2]/winnerCount is not a number (even after removing end 'w') or args[2] does not end with 'w', condition returns:
+            return message.channel.send('Please provide the number of winners for the giveaway! E.g. `3w`');
+                if ((args[2].toString().slice(0, -1)) <= 0)   
+                    return message.channel.send('The number of winners cannot be less than 1!');
+
+            let giveawayChannel = message.mentions.channels.first();
+            if (!giveawayChannel || !args[3]) return message.channel.send("Please provide a valid channel to start the giveaway!")
+
+            let prize = args.slice(4).join(" ");
+            if (!prize) return message.channel.send('Please provide a prize to start the giveaway!');
+
+            let startGiveawayEmbed = new MessageEmbed()
+                .setTitle("🎉 GIVEAWAY 🎉")
+                .setDescription(`${prize}\n\nReact with 🎉 to participate in the giveaway!\nWinners: **${winnerCount.toString().slice(0, -1)}**\nTime Remaining: **${duration}**\nHosted By: **${message.author}**`)
+                .setColor('#FFFFFF')
+                .setTimestamp(Date.now() + ms(args[1])) 
+                .setFooter("Giveaway ends"); 
+
+            let embedGiveawayHandle = await giveawayChannel.send({embeds: [startGiveawayEmbed]})
+            embedGiveawayHandle.react("🎉").catch(console.error); 
+
+            setTimeout(() => {
+                if (embedGiveawayHandle.reactions.cache.get("🎉").count <= 1) {
+                    return giveawayChannel.send("Nobody joined the giveaway :(")
+                }
+                if (embedGiveawayHandle.reactions.cache.get("🎉").count <= winnerCount.toString().slice(0, -1)) { // this if-statement can be removed
+                    return giveawayChannel.send("There's not enough people in the giveaway to satisfy the number of winners!")
+                }
+
+                let winner = embedGiveawayHandle.reactions.cache.get("🎉").users.cache.filter((users) => !users.bot).random(winnerCount.toString().slice(0, -1)); 
+
+                const endedEmbedGiveaway = new MessageEmbed()
+                .setTitle("🎉 GIVEAWAY 🎉")
+                .setDescription(`${prize}\n\nWinner(s): ${winner}\nHosted By: **${message.author}**\nWinners: **${winnerCount.toString().slice(0, -1)}**\nParticipants: **${embedGiveawayHandle.reactions.cache.get("🎉").count - 1}**\nDuration: **${duration}**`)
+                .setColor('#FFFFFF')
+                .setTimestamp(Date.now() + ms(args[1]))  
+                .setFooter("Giveaway ended"); 
+
+                embedGiveawayHandle.edit({embeds:[endedEmbedGiveaway]}); 
+                const congratsEmbedGiveaway = new MessageEmbed()
+                .setDescription(`🥳 Congratulations ${winner}! You just won **${prize}**!`)
+                .setColor('#FFFFFF')
+
+                giveawayChannel.send({embeds: [congratsEmbedGiveaway]}).catch(console.error); 
+            }, ms(args[1]));
+
+        } 
+    }
+})
 
 
 if(cmd === `${prefix}calc`){
