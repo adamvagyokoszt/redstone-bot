7e8fba5f905e2bd4be11bf1f9bfcb528f893e37f
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-02-17 21:51:32 +0100

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -2,27 +2,29 @@ const { readdirSync } = require("fs");
 
 const ascii = require("ascii-table");
 
-let table = new ascii("Parancsok");
-table.setHeading("Parancsok", "Statusz");
+
+let table = new ascii("Commands");
+table.setHeading("Command", "Load status");
 
 module.exports = (client) => {
-    readdirSync("./commands/").forEach(dir => {
-        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
 
-        for(let file of commands) {
-            let pull = require(`../commands/${dir}/${file}`);
+  readdirSync("./commands/").forEach(dir => {
 
-            if(pull.name){
-                client.commands.set(pull.name, pull)
-                table.addRow(file, 'Működik');
-            } else {
-                table.addRow(file, 'Hiba');
-                continue;
-            }
+    const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
+    for (let file of commands) {
+      let pull = require(`../commands/${dir}/${file}`);
 
-            if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
-        }
-    });
+      if (pull.name) {
+        client.commands.set(pull.name, pull);
+        table.addRow(file, '✅');
+      } else {
+        table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
+        continue;
+      }
 
-    console.log(table.toString());
+      if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
+    }
+  });
+
+  console.log(table.toString());
 }
