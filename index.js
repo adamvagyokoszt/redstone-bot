1d58a613de0706a2a8f2d47fa6545d8fbbefcc96
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-02-17 16:17:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,28 @@ client.on("ready", async() => {
         client.user.setActivity(status, {type: "WATCHING"})
     }, 5000)
 })
+///////handler///////
+
+client.on("message", async message => {
+    let prefix = botconfig.prefix;
+
+    if(message.author.client) return;
+    if(!message.guild) return;
+    if(!message.content.startsWith(prefix)) return;
+    if(!message.member) message.member = await message.guild.fetchMember(message)
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if(cmd.length === 0) return;
+
+    let command = client.commands.get(cmd);
+    if(!command) command = client.commands.get(client.aliases.get(cmd));
+
+    if(command)
+    command.run(client, message, args);
+});
+
 /////) Üdvözlő rendszer/////////
 
 const welcomeChannelId = "730430575046819914"
