654e3b34a72a8b8c17795fcae35a2b6687bc6859
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-02-17 10:08:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -393,7 +393,60 @@ const embed = new MessageEmbed()
 message.channel.send({ embeds: [embed] })
             
 } 
+if(cmd === `${prefix}giveaway`){
+            const messageArray = message.content.split(" ");
+            if (!message.member.permissions.has('ADMINISTRATOR')) return message.channel.send("Ehhez a parancshoz nincs jogod!")
 
+            let tárgy = "";
+            let idő;
+            let winnerCount;
+
+            for (let i = 1; i < args.length; i++){
+                tárgy += (args[i] + " ")
+                console.log(tárgy)
+            }
+
+            idő = args[0];
+
+        if(!idő){
+            return message.reply("Kérlek adj meg egy idő intervallumot! pl: 100s, 5h, 2d")
+        }
+        if(!tárgy){
+            return message.reply("Kérlek add meg a nyereményjáték tárgyát!")
+        }
+
+        var Gembed = new MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle("Nyereményjáték!!!!")
+        .setDescription(`**${tárgy}**`)
+        .addField("`Időtartam:`", ms(ms(idő), {long: true}), true)
+        .setFooter("A jelentkezéshe reagálj ezzel: 🎉")
+        var embedSend = await message.channel.send({ embeds: [Gembed] });
+        embedSend.react("🎉");
+
+        setTimeout(async() => {
+            try{
+                const peopleReactedBOT =  await embedSend.reactions.cache.get("🎉").users.fetch();
+                var peopleReacted = peopleReactedBOT.array().filter(u => u.id !== bot.user.id);
+            }catch(e){
+                return message.channel.send(`Hiba törtét a **${tárgy}** sorsolása során! Hiba: `+"`"+e+"`")
+            }
+            var winner;
+
+            if(peopleReacted.length <= 0){
+                return message.channel.send("Senki nem jelentkezett a nyereményjátékra! :C")
+            } else {
+                var index = Math.floor(Math.random() * peopleReacted.length);
+                winner = peopleReacted[index]
+            }
+
+            if(!winner) {
+                message.channel.send("Hiba történt a sorsolás során!")
+            } else {
+                message.channel.send(`🎉🎉🎉🎉 **${winner.toString()}** megnyerte ezt: **${tárgy}**`);
+            }
+        }, ms(idő))
+        }
     
     if(cmd === `${prefix}weather`){
         if(args[0]){
