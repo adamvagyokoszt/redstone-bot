30a869e7cd0b000e05c22e84560cdb30adb8d308
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-02-18 19:28:20 +0100

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,44 +1,30 @@
-const { reddirSync, readdirSync } = require ("fs");
+const { readdirSync } = require("fs");
 
-const ascii = require ("ascii-table");
+const ascii = require("ascii-table");
 
-let table = new ascii("parancsok");
-table.setHeading("Parancsok", "Státusz");
+
+let table = new ascii("Parancsok");
+table.setHeading("Parancs", "Állapot");
 
 module.exports = (client) => {
-    readdirSync("./commands/").forEach(dir => {
-        const commands = readdirSync(`./commands/${dir}`).filter(file => file.endsWith(".js"));
 
-        for(let file of commands) {
-            let pull = require(`../commands/${dir}/${file}`);
+  readdirSync("./commands/").forEach(dir => {
 
-            if(pull.name){
-                client.commands.set(pull.name, pull)
-                table.addRow(file, 'Működik');
-            } else {
-                table.addRow(file, 'Hiba');
-                continue
-            }
-
-            if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
-        }
-    });
-
-    console.log(table.toString());
-}
-readdirSync("./events/").forEach((file) => {
-    const events = readdirSync("./events/").filter((file) =>
-      file.endsWith(".js")
-    );
-
-    for (let file of events) {
-      let pull = require(`../events/${file}`);
+    const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
+    for (let file of commands) {
+      let pull = require(`../commands/${dir}/${file}`);
 
       if (pull.name) {
-        client.events.set(pull.name, pull);
+        client.commands.set(pull.name, pull);
+        table.addRow(file, 'Müködik');
       } else {
+        table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
         continue;
       }
-    }
-  }); 
 
+      if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
+    }
+  });
+
+  console.log(table.toString());
+}
