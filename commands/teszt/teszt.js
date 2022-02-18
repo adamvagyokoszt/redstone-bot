72108ba2903057803340a9151db816bf5baef6f8
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-02-18 18:38:54 +0100

    Update teszt.js

--- commands/teszt/teszt.js
+++ commands/teszt/teszt.js
@@ -1,3 +1,5 @@
+const { Client, Intents, MessageEmbed, Permissions, CommandInteraction, ReactionUserManager, MessageActionRow, MessageButton, Collection } = require("discord.js")
+const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.DIRECT_MESSAGE_TYPING,Intents.FLAGS.GUILD_MEMBERS,Intents.FLAGS.GUILD_PRESENCES,Intents.FLAGS.GUILD_MESSAGES] });
 module.exports = {
     name: "teszt",
     category: "teszt kategória",
