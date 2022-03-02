e5cb78c3c106a449694cfaa9c0ec9840f2c3fc7e
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-03-02 19:05:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ module.exports = client;
 // Global Variables
 client.commands = new Collection();
 client.slashCommands = new Collection();
-client.config = require("./config.json");
+client.config = require("./botconfig.json");
 
 // Initializing the project
 require("./handler")(client);
