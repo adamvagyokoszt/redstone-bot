356b183a2f8acccf5b5dbd8e5217395d10a6b830
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-03-02 18:59:22 +0100

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,30 +1,52 @@
-const { readdirSync } = require("fs");
+const { glob } = require("glob");
+const { promisify } = require("util");
+const { Client } = require("discord.js");
+const mongoose = require("mongoose");
 
-const ascii = require("ascii-table");
+const globPromise = promisify(glob);
 
+/**
+ * @param {Client} client
+ */
+module.exports = async (client) => {
+    // Commands
+    const commandFiles = await globPromise(`${process.cwd()}/commands/**/*.js`);
+    commandFiles.map((value) => {
+        const file = require(value);
+        const splitted = value.split("/");
+        const directory = splitted[splitted.length - 2];
+if (file.name) {
+            const properties = { directory, ...file };
+            client.commands.set(file.name, properties);
+        }
+    });
 
-let table = new ascii("Parancsok");
-table.setHeading("Parancs", "Állapot");
+    // Events
+    const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
+    eventFiles.map((value) => require(value));
 
-module.exports = (client) => {
-
-  readdirSync("./commands/").forEach(dir => {
-
-    const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
-    for (let file of commands) {
-      let pull = require(`../commands/${dir}/${file}`);
-
-      if (pull.name) {
-        client.commands.set(pull.name, pull);
-        table.addRow(file, 'Müködik');
-      } else {
-        table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
-        continue;
-      }
-
-      if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
-    }
-  });
-
-  console.log(table.toString());
-}
+    // Slash Commands
+const slashCommands = await globPromise(
+        `${process.cwd()}/SlashCommands/*/*.js`
+    );
+const arrayOfSlashCommands = [];
+    slashCommands.map((value) => {
+        const file = require(value);
+        if (!file?.name) return;
+        client.slashCommands.set(file.name, file);
+if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
+        arrayOfSlashCommands.push(file);
+    });
+    client.on("ready", async () => {
+        // Register for a single guild
+        await client.guilds.cache
+            .get("replace this with your guild id")
+            .commands.set(arrayOfSlashCommands);
+// Register for all the guilds the bot is in
+        // await client.application.commands.set(arrayOfSlashCommands);
+    });
+// mongoose
+    const { mongooseConnectionString } = require('../config.json')
+    if (!mongooseConnectionString) return;
+mongoose.connect(mongooseConnectionString).then(() => console.log('Connected to mongodb'));
+};
